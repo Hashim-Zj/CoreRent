c5428f9c83f8d4e4e9de3157b90237538d6e6f02
Hashim-Zj <hashimmohammedtp@gmail.com>
2022-06-22 15:53:00 +0530

    Saparate admin and users

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var adminRouter = require('./routes/admin');
+const { extname } = require('path');
+const { engine } = require('express-handlebars');
+//var hbs=require('express-handlebars');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs', engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials/' }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,15 +23,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products=[
+    {
+      name:"Driller",
+      category:"Tools",
+      description:"Tis is toolbox",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtztBp51xeePPTQPknv4xeDBGCADsyEnb19cP1HNk72A&s"
+    },
+    {
+      name:"Driller",
+      category:"Tools",
+      description:"Tis is toolbox",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0gnWC2KSp61fWG5eeUwVtk6U5eUxmC3-d7do9_XNM9w&s",
+    },
+    {
+      name:"Driller",
+      category:"Tools",
+      description:"Tis is toolbox",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZb-1AbF_gT6MiZzZQtXxZ0lqP_nXMCD083cE1gGNq8Q&s",
+    },
+    {
+      name:"Driller",
+      category:"Tools",
+      description:"Tis is toolbox",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRLa4yVXvyxBmxNI5S_kcK3IB0X5GtZgiVxw&usqp=CAU",
+    },
+
+  ]
+  res.render('index', {products,admin:false})
 });
 
 module.exports = router;
